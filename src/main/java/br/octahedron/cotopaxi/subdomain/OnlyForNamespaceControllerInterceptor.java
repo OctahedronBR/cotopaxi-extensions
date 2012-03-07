c3f8313a0f0f902828730405d802114daf6bbbde
Danilo Queiroz <dpenna.queiroz@gmail.com>
2012-03-06 23:19:34 -0300

    Classes imported from figgo project

---
+++ src/main/java/br/octahedron/cotopaxi/subdomain/OnlyForNamespaceControllerInterceptor.java
@@ -0,0 +1,58 @@
+/*
+ *  This file is part of Cotopaxi.
+ *
+ *  Cotopaxi is free software: you can redistribute it and/or modify
+ *  it under the terms of the Lesser GNU General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  Cotopaxi is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Lesser GNU General Public License for more details.
+ *
+ *  You should have received a copy of the Lesser GNU General Public License
+ *  along with Cotopaxi. If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.octahedron.cotopaxi.subdomain;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import br.octahedron.cotopaxi.controller.Controller;
+import br.octahedron.cotopaxi.interceptor.ControllerInterceptor;
+import br.octahedron.util.Log;
+
+/**
+ * This interceptor only allows the controller to be executed if the subdomain isn't equals to
+ * 'www'. If it is, it fires a not found.
+ * 
+ * To use it just added the {@link OnlyForNamespace} annotation to {@link Controller} class/method
+ * 
+ * @author Danilo Queiroz - dpenna.queiroz@gmail.com
+ */
+public class OnlyForNamespaceControllerInterceptor extends ControllerInterceptor {
+	
+	private static final Log log = new Log(OnlyForNamespaceControllerInterceptor.class); 
+
+	@Retention(value = RetentionPolicy.RUNTIME)
+	@Target(value = {ElementType.METHOD, ElementType.TYPE})
+	public @interface OnlyForNamespace {
+	}
+
+	@Override
+	public void execute(Annotation arg0) {
+		if (this.subDomain().equalsIgnoreCase("www")) {
+			log.debug("Invalid address for www");
+			this.notFound();
+		}
+	}
+
+	@Override
+	public Class<? extends Annotation> getInterceptorAnnotation() {
+		return OnlyForNamespace.class;
+	}
+}
